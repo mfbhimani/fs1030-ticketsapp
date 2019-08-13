d4193c0cc05a16bd1019158e98b28deb79399d83
Mohammed Bhimani <mfbhimani@gmail.com>
2019-08-12 21:43:04 -0400

    updated  env file and updated app.js

--- app.js
+++ app.js
@@ -4,19 +4,20 @@ const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const path = require('path');
 const app = express();
+require('dotenv').config();
 
 const {getHomePage} = require('./routes/index');
 const {addCustomerPage, addCustomer} = require('./routes/ticketing');
 // const {addPlayerPage, addPlayer, deletePlayer, editPlayer, editPlayerPage} = require('./routes/player');
 
-const port = 5000;
+// const port = 5000;
 
 // create connection to database
 // the mysql.createConnection function takes in a configuration object which contains host, user, password and the database name.
 const db = mysql.createConnection ({
     /* host: 'localhost',
     user: 'root',
-    password: 'Mf786114@',
+    password: '',
     database: 'assg2tickets' */
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
@@ -34,7 +35,9 @@ db.connect((err) => {
 global.db = db;
 
 // configure middleware
-app.set('port', process.env.port || port); // set express to use this port
+// app.set('port', process.env.port || port); // set express to use this port
+app.set('port', process.env.HTTP_PORT || port);
+// app.set('port', '5000');
 app.set('views', __dirname + '/views'); // set express to look in this folder to render our view
 app.set('view engine', 'ejs'); // configure template engine
 app.use(bodyParser.urlencoded({ extended: false }));
